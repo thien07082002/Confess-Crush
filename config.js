ca68d341b985ad24ed2989a48dee5e95b409ecab
thien07082002 <124779882+thien07082002@users.noreply.github.com>
2023-02-08 18:52:03 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,17 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "Chào thảo đến trang web của thiện",
+    introTitle: 'Nguyễn Dương Phương ',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
+    btnIntro: 'Thảo à thiện muốn nói rằng đã thích thảo từ lâu',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
-}
+    question: 'Trên thế giới  8 tỉ người mà sao thiện cứ bị thích thảo😘 <3',
+    btnReply: 'Thích thảo lâu lắm rồi ó',
+    reply: Liệu thảo có mún trở thành người yêu của thiện khum?',
+    mess: 'Yes (Y) 🥰. No (N) :(',
+   
